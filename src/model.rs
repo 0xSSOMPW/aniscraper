44190594f370a89ce1a0b2bc3456e48f79e62e3e
falcon71181 <abhaythakur71181@gmail.com>
2024-08-27 00:01:23 +0530

    feat: home latest episodes

--- src/model.rs
+++ src/model.rs
@@ -6,3 +6,15 @@ pub struct MinimalAnime {
     pub title: String,
     pub image: String,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Anime {
+    pub id: String,
+    pub title: String,
+    pub subs: u32,
+    pub dubs: u32,
+    pub eps: u32,
+    pub duration: String,
+    pub rating: String,
+    pub image: String,
+}
