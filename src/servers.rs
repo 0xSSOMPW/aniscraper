3286a6ebff0681c63b0e56b4753f8e0a049858ec
falcon71181 <abhaythakur71181@gmail.com>
2024-09-14 13:18:18 +0530

    feat: megacloud utility methods

--- src/servers.rs
+++ src/servers.rs
@@ -2,10 +2,7 @@ use regex::Regex;
 use serde_json::Value;
 
 use crate::{error::AniRustError, proxy::Proxy, utils::get_curl};
-use std::{
-    env::vars,
-    time::{SystemTime, UNIX_EPOCH},
-};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct Track {
     pub file: String,
@@ -112,3 +109,112 @@ impl EpisodeType {
 }
 
 pub struct MegaCloudServer;
+
+impl MegaCloudServer {
+    pub async fn extract(video_url: &str, proxies: &[Proxy]) -> Result<(), AniRustError> {
+        let mut encrypted_string = String::new();
+        let video_id = video_url
+            .split('/')
+            .last()
+            .and_then(|s| s.split('?').next())
+            .unwrap_or_default();
+
+        let url = format!("{}{}", MEGACLOUD.sources, video_id);
+
+        let curl = get_curl(&url, proxies).await?;
+        // // Parse the string as JSON
+        let json_value = serde_json::from_str::<Value>(&curl).unwrap_or_default();
+
+        if let Some(data) = json_value.get("sources") {
+            encrypted_string =
+                serde_json::from_str::<String>(data.to_string().as_str()).unwrap_or_default();
+        }
+
+        // Get the current time in milliseconds since UNIX_EPOCH
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis();
+
+        // Format the URL by concatenating the base URL with the current timestamp
+        let base_url = "https://megacloud.tv/js/player/a/prod/e1-player.min.js?v=";
+        let full_url = format!("{}{}", base_url, now);
+
+        let curl = get_curl(&full_url, proxies).await?;
+        let variables = extract_variables(&curl)?;
+        if variables.is_empty() {
+            return Err(AniRustError::UnknownError(
+                "Can't find variables. Perhaps the extractor is outdated.".to_string(),
+            ));
+        }
+        println!("{:?}", get_secret(&encrypted_string, &variables));
+
+        Ok(())
+    }
+}
+
+fn extract_variables(text: &str) -> Result<Vec<(u32, u32)>, AniRustError> {
+    let regex = Regex::new(r"case\s*0x[0-9a-f]+:\s*\w+\s*=\s*(\w+)\s*,\s*\w+\s*=\s*(\w+);")?;
+
+    let vars: Vec<(u32, u32)> = regex
+        .captures_iter(text)
+        .filter_map(|cap| {
+            if cap[0].contains("partKey") {
+                return None;
+            }
+
+            let match_key1 = matching_key(&cap[1], text).ok()?;
+            let match_key2 = matching_key(&cap[2], text).ok()?;
+
+            match (
+                u32::from_str_radix(&match_key1, 16),
+                u32::from_str_radix(&match_key2, 16),
+            ) {
+                (Ok(key1), Ok(key2)) => Some((key1, key2)),
+                _ => None,
+            }
+        })
+        .collect();
+
+    Ok(vars)
+}
+
+fn matching_key(value: &str, script: &str) -> Result<String, AniRustError> {
+    let regex = Regex::new(&format!(r",{}=(((?:0x)?[0-9a-fA-F]+))", value))?;
+    if let Some(captures) = regex.captures(script) {
+        let match_str = captures
+            .get(1)
+            .ok_or_else(|| AniRustError::UnknownError("Failed to capture key".to_string()))?
+            .as_str();
+        Ok(match_str.trim_start_matches("0x").to_string())
+    } else {
+        Err(AniRustError::UnknownError(
+            "Failed to match the key".to_string(),
+        ))
+    }
+}
+
+fn get_secret(encrypted_string: &str, values: &Vec<(u32, u32)>) -> (String, String) {
+    let mut secret = String::new();
+    let mut encrypted_source_array: Vec<char> = encrypted_string.chars().collect();
+    let mut current_index: usize = 0;
+
+    for &(start_offset, length) in values {
+        let start = start_offset as usize + current_index;
+        let end = start + length as usize;
+        for i in start..end {
+            if let Some(ch) = encrypted_string.chars().nth(i) {
+                secret.push(ch);
+                encrypted_source_array[i] = '\0';
+            }
+        }
+        current_index += length as usize;
+    }
+
+    let encrypted_source: String = encrypted_source_array
+        .into_iter()
+        .filter(|&c| c != '\0')
+        .collect();
+
+    (secret, encrypted_source)
+}
